f88e3edf8c9598d607f5014b23a31e4fabb49375
syafiq@redapesolutions.com <syafiq@redapesolutions.com>
2020-05-30 22:01:33 +0800

    changes

---
+++ masak2/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField('Email address', unique=True)
+
+    def __str__(self):
+        return self.username
