0655ec0afb17a01071e50af5254ddfa28a26bfbd
syafiq@redapesolutions.com <syafiq@redapesolutions.com>
2020-05-31 18:16:49 +0800

    add models to admin page

--- masak2/recipes/admin.py
+++ masak2/recipes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, IngredientName, IngredientUnit, Ingredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientNameAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientUnitAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(IngredientUnit, IngredientUnitAdmin)
+admin.site.register(IngredientName, IngredientNameAdmin)
+admin.site.register(Recipe, RecipeAdmin)
