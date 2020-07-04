33e010ee4c5f0b8a8821a7497d3ce6a2578d3d32
syafiq <syafiq.termizi@omesti.com>
2020-07-04 16:12:01 +0800

    add steps to admin

--- masak2/steps/admin.py
+++ masak2/steps/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Step
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Step, StepAdmin)
