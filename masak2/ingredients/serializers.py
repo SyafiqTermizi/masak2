271ea39ecf7c01a5988d7b43132f6572f5367f7c
syafiq <syafiq.termizi@omesti.com>
2020-07-05 05:50:28 +0800

    add missing fields in serializer

--- masak2/ingredients/serializers.py
+++ masak2/ingredients/serializers.py
@@ -4,16 +4,23 @@ from rest_framework import serializers
 from .models import IngredientGroup, IngredientName, IngredientUnit, Ingredient
 
 
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IngredientGroup
-        fields = ("id", "name", "recipe")
-
-
 class IngredientSerializer(serializers.ModelSerializer):
     unit = serializers.CharField()
     name = serializers.CharField()
 
     class Meta:
         model = Ingredient
-        fields = ("id", "name", "unit", "note", "group")
+        fields = ("id", "name", "unit", "note", "group", "amount")
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True)
+
+    class Meta:
+        model = IngredientGroup
+        fields = (
+            "id",
+            "name",
+            "recipe",
+            "ingredients",
+        )
--- masak2/recipes/serializers.py
+++ masak2/recipes/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+from ingredients.serializers import GroupSerializer
+from steps.serializers import StepSerializer
+
 from .models import Recipe, Media
 
 
@@ -12,7 +15,18 @@ class MediaSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     created_by = serializers.CharField()
     medias = MediaSerializer(many=True)
+    groups = GroupSerializer(many=True)
+    steps = StepSerializer(many=True)
 
     class Meta:
         model = Recipe
-        fields = ("id", "name", "description", "difficulty", "created_by", "medias")
+        fields = (
+            "id",
+            "name",
+            "description",
+            "difficulty",
+            "created_by",
+            "medias",
+            "groups",
+            "steps",
+        )
