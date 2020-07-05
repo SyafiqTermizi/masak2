4128b74778d23218e653bc1d458ed7b719a72c4e
syafiq <syafiq.termizi@omesti.com>
2020-07-05 19:15:54 +0800

    remove unused endpoint

--- masak2/config/api.py
+++ masak2/config/api.py
@@ -1,13 +1,8 @@
 from rest_framework import routers
 
-from steps.views import StepViewSet
-from ingredients.views import IngredientViewSet, GroupViewSet
 from recipes.views import RecipeViewSet
 
 router = routers.DefaultRouter()
-router.register(r"steps", StepViewSet)
-router.register(r"ingredients", IngredientViewSet)
-router.register(r"groups", GroupViewSet)
 router.register("recipes", RecipeViewSet)
 
 urlpatterns = router.urls
--- masak2/steps/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
-
-from .models import Step
-from .serializers import StepSerializer
-
-
-class StepViewSet(ReadOnlyModelViewSet):
-    queryset = Step.objects.all()
-    serializer_class = StepSerializer
