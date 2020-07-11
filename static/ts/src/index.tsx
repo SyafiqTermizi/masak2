9b33c865726e0c7bb1e449a38451066ef34541fe
syafiq <syafiq.termizi@omesti.com>
2020-07-12 01:10:02 +0800

    add react router

--- static/ts/src/index.tsx
+++ static/ts/src/index.tsx
@@ -2,17 +2,25 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "@syafiqtermizi/masak2-store";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Navbar } from "./components/Navbar";
 import Recipes from "./pages/Recipes";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Elem = () => (
   <Provider store={store}>
-    <Navbar />
-    <div className="container">
-      <Recipes />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/">
+            <Recipes />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
 );
 
