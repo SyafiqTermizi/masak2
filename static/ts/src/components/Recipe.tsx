3b9621e97083390898bc961291d83fbac8b947a2
syafiq <syafiq.termizi@omesti.com>
2020-07-16 04:49:29 +0800

    Fix image height

--- static/ts/src/components/Recipe.tsx
+++ static/ts/src/components/Recipe.tsx
@@ -22,7 +22,9 @@ export const Recipe: React.FC<Props> = ({
 }) => (
   <div className="col-3">
     <div className="card">
-      {medias.length >= 1 && <img src={medias[0].media} alt="" />}
+      {medias.length >= 1 && (
+        <img src={medias[0].media} alt={name} height="300px" />
+      )}
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text"></p>
