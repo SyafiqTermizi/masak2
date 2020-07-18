c76bde04fd41f77d80fbef7bc8c8c189ab20cddf
syafiq <syafiq.termizi@omesti.com>
2020-07-19 03:01:02 +0800

    fix typo

--- masak2/ingredients/tests/test_serializers.py
+++ masak2/ingredients/tests/test_serializers.py
@@ -5,7 +5,7 @@ from ingredients.serializers import IngredientSerializer
 
 def test_serialize_ingredient_serializer(ingredient, ingredient_data):
     """
-    Ingredient serilizer should serialize ingredient instance correctly
+    IngredientSerializer should serialize ingredient instance correctly
     """
     serialized_ingredient = IngredientSerializer(instance=ingredient)
     assert serialized_ingredient.data == ingredient_data
@@ -13,7 +13,7 @@ def test_serialize_ingredient_serializer(ingredient, ingredient_data):
 
 def test_deserialize_ingredient_serializer(ingredient_data):
     """
-    Ingredient serilizer should deserialize ingredient instance correctly
+    IngredientSerializer should deserialize ingredient instance correctly
     """
     deserialized_data = IngredientSerializer(data=ingredient_data)
     assert deserialized_data.is_valid()
@@ -27,7 +27,7 @@ def test_deserialize_ingredient_serializer(ingredient_data):
         ({"amount": "amount", "unit": "unit"}, False,),
     ),
 )
-def test_deserialize_ingredient_serializer_exeption(data, is_valid):
+def test_ingredient_serializer_fields(data, is_valid):
     """
     test whether each data passed to serializer is valid
     """
