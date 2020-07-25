51aa1877d7fb9416f124c6655723233fafeb309e
syafiq <syafiq.termizi@omesti.com>
2020-07-26 01:02:46 +0800

    add unit test for navbar

--- static/ts/src/components/navbar.test.tsx
+++ static/ts/src/components/navbar.test.tsx
@@ -4,22 +4,70 @@ import { render, fireEvent } from "@testing-library/react";
 
 import { Navbar } from "./Navbar";
 
-test("click Home should navigate to /", () => {
-  const { getByText } = render(
-    <HashRouter>
-      <Navbar />
-    </HashRouter>
-  );
-  const elem = getByText("Home");
-  expect(elem.innerHTML).toBe("Home");
+describe("Navbar link for unauthenticated user should have the correct content", () => {
+  beforeEach(() => {
+    window.user = {
+      isAuthenticated: false,
+      username: "test",
+    };
+  });
+
+  it("should contain link to Home", () => {
+    const { getByText } = render(
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
+    );
+    const elem = getByText("Home");
+    expect(elem.innerHTML).toBe("Home");
+  });
+
+  it("should contain link to Login page", () => {
+    const { getByText } = render(
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
+    );
+    const elem = getByText("Login");
+    expect(elem.innerHTML).toBe("Login");
+  });
 });
 
-test("click Create should navigate to /create", () => {
-  const { getByText } = render(
-    <HashRouter>
-      <Navbar />
-    </HashRouter>
-  );
-  const elem = getByText("Create");
-  expect(elem.innerHTML).toBe("Create");
+describe("Navbar links for authenticated user should have the correct content", () => {
+  beforeEach(() => {
+    window.user = {
+      isAuthenticated: true,
+      username: "test",
+    };
+  });
+
+  it("should contain link to Home", () => {
+    const { getByText } = render(
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
+    );
+    const elem = getByText("Home");
+    expect(elem.innerHTML).toBe("Home");
+  });
+
+  it("should contain link to Create page", () => {
+    const { getByText } = render(
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
+    );
+    const elem = getByText("Create");
+    expect(elem.innerHTML).toBe("Create");
+  });
+
+  it("should contain link to Logout page", () => {
+    const { getByText } = render(
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
+    );
+    const elem = getByText("Logout");
+    expect(elem.innerHTML).toBe("Logout");
+  });
 });
