5f71c79e8a2c3e1e13ec1bba415c7892a502aa4b
syafiq <syafiq.termizi@omesti.com>
2020-08-09 23:00:23 +0800

    Add search that works

---
+++ static/ts/src/components/Searchbar.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+  searchTerm: string;
+  setSearchTerm: (param: string) => void;
+  handleSearch: () => void;
+  clearSearchTerm: () => void;
+}
+
+export const SearchBar: React.FC<Props> = ({
+  searchTerm,
+  setSearchTerm,
+  handleSearch,
+  clearSearchTerm,
+}) => (
+  <div className="row mt-5 mb-3">
+    <div className="input-group mb-3 search-bar">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search Recipes or ingredients"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
+          }
+        }}
+      />
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+        onClick={() => {
+          if (searchTerm) {
+            clearSearchTerm();
+          } else {
+            handleSearch();
+          }
+        }}
+      >
+        {searchTerm ? <b>Clear filter</b> : <FontAwesomeIcon icon={faSearch} />}
+      </button>
+    </div>
+  </div>
+);
--- static/ts/src/pages/Recipes.tsx
+++ static/ts/src/pages/Recipes.tsx
@@ -4,31 +4,44 @@ import { connect } from "react-redux";
 import { StateTree } from "@syafiqtermizi/masak2-store";
 import { MediaState } from "@syafiqtermizi/masak2-store/lib/medias";
 import {
+  setSearchTerm,
+  clearSearchTerm,
+} from "@syafiqtermizi/masak2-store/lib/search";
+import {
   retrieveRecipes,
   RecipeState,
 } from "@syafiqtermizi/masak2-store/lib/recipes";
 
 import { Recipe } from "../components/Recipe";
+import { SearchBar } from "../components/Searchbar";
 
 interface Props {
   recipes: RecipeState;
   medias: MediaState;
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+  clearSearchTerm: () => void;
   retrieveRecipes: () => any;
 }
 
 export const Recipes: React.FC<Props> = ({
   recipes,
   medias,
+  searchTerm,
+  setSearchTerm,
+  clearSearchTerm,
   retrieveRecipes,
 }) => {
+  const handleSearch = () => {
+    if (searchTerm.length > 2) retrieveRecipes();
+  };
+  const clearSearch = () => {
+    clearSearchTerm();
+    retrieveRecipes();
+  };
+
   useEffect(() => {
-    if (
-      Object.keys(recipes).length === 1 &&
-      recipes[1] &&
-      recipes[1].name === ""
-    ) {
-      retrieveRecipes();
-    }
+    retrieveRecipes();
   }, []);
 
   const elem = Object.keys(recipes).map((id) => {
@@ -48,16 +61,36 @@ export const Recipes: React.FC<Props> = ({
     );
   });
 
-  return <div className="row mt-5 recipes-container">{elem}</div>;
+  return (
+    <>
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        handleSearch={() => handleSearch()}
+        clearSearchTerm={() => clearSearch()}
+      />
+      {searchTerm && (
+        <div className="row">
+          <div className="col-12 mb-3">
+            <h3>You are searching for "{searchTerm}"</h3>
+          </div>
+        </div>
+      )}
+      <div className="row recipes-container">{elem}</div>
+    </>
+  );
 };
 
 const mapStateToProps = (state: StateTree) => ({
   recipes: state.recipe,
   medias: state.media,
+  searchTerm: state.search.searchTerm,
 });
 
 const mapDispatchToProps = {
   retrieveRecipes,
+  setSearchTerm,
+  clearSearchTerm,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Recipes);
