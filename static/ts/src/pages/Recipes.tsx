fe43b50c3892a45c062f0793fedf9d4f9bd79182
syafiq <syafiq.termizi@omesti.com>
2020-08-09 12:25:34 +0800

    remove old search

--- static/ts/src/pages/Recipes.tsx
+++ static/ts/src/pages/Recipes.tsx
@@ -6,43 +6,21 @@ import { MediaState } from "@syafiqtermizi/masak2-store/lib/medias";
 import {
   retrieveRecipes,
   RecipeState,
-  searchRecipe,
 } from "@syafiqtermizi/masak2-store/lib/recipes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
-import { SearchBar } from "../components/SearchBar";
 import { Recipe } from "../components/Recipe";
 
 interface Props {
   recipes: RecipeState;
   medias: MediaState;
   retrieveRecipes: () => any;
-  searchRecipe: (searchTerm: string) => void;
 }
 
 export const Recipes: React.FC<Props> = ({
   recipes,
   medias,
   retrieveRecipes,
-  searchRecipe,
 }) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [isFiltered, setIsFiltered] = useState<Boolean>(false);
-
-  const handleSearch = () => {
-    if (searchTerm && !isFiltered) {
-      searchRecipe(searchTerm);
-      setIsFiltered(true);
-    }
-  };
-
-  const clearFilter = () => {
-    setSearchTerm("");
-    setIsFiltered(false);
-    retrieveRecipes();
-  };
-
   useEffect(() => {
     if (
       Object.keys(recipes).length === 1 &&
@@ -70,34 +48,7 @@ export const Recipes: React.FC<Props> = ({
     );
   });
 
-  return (
-    <>
-      <div className="row mt-5 mb-2">
-        <div className="mt-1 col-6">
-          {isFiltered ? (
-            <h2>
-              Recipes containing "{searchTerm}" &nbsp;
-              <button className="btn btn-light" onClick={clearFilter}>
-                <FontAwesomeIcon icon={faTimes} />
-              </button>
-            </h2>
-          ) : (
-            <SearchBar
-              handleInput={setSearchTerm}
-              handleSearch={handleSearch}
-            />
-          )}
-        </div>
-        <div className="col-6 text-right">
-          <button className="btn btn-light" onClick={clearFilter}>
-            Refresh &nbsp;
-            <FontAwesomeIcon icon={faSyncAlt} />
-          </button>
-        </div>
-      </div>
-      <div className="row recipes-container">{elem}</div>
-    </>
-  );
+  return <div className="row mt-5 recipes-container">{elem}</div>;
 };
 
 const mapStateToProps = (state: StateTree) => ({
@@ -107,7 +58,6 @@ const mapStateToProps = (state: StateTree) => ({
 
 const mapDispatchToProps = {
   retrieveRecipes,
-  searchRecipe,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Recipes);
