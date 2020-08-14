cdfb47abb022ded75ac6eaa57e35bba8f7bba044
syafiq <syafiq.termizi@omesti.com>
2020-08-14 11:07:19 +0800

    add image field to tags

--- masak2/tags/models.py
+++ masak2/tags/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+    image = models.FileField(upload_to="tags", blank=True, default="")
 
     def __str__(self):
         return self.name
