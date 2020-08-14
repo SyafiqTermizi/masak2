142db711eed43585caa84a8dbb3b9a76eda1343e
syafiq <syafiq.termizi@omesti.com>
2020-08-14 11:56:09 +0800

    add image to serializer

--- masak2/tags/serializers.py
+++ masak2/tags/serializers.py
@@ -6,4 +6,4 @@ from .models import Tag
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ("id", "name")
+        fields = ("id", "name", "image")
