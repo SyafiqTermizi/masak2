99f5c5dbdd12f3f5e5e92a35aa3fcbeb1b8f1903
syafiq <syafiq.termizi@omesti.com>
2020-08-19 22:53:22 +0800

    test

--- static/ts/src/components/tag.test.tsx
+++ static/ts/src/components/tag.test.tsx
@@ -4,7 +4,8 @@ import { render } from "@testing-library/react";
 import { Tag } from "./Tag";
 
 test("Tag component should render correctly with given props", () => {
-  const { getByTestId } = render(<Tag tag="test" />);
+  const func = () => null;
+  const { getByTestId } = render(<Tag handleClick={func} tag="test" />);
   const tag = getByTestId("tag");
   expect(tag.innerHTML).toBe("test");
 });
