3bb0a9f4c883aa1e647c1c94e7979914ea25b0db
syafiq <syafiq.termizi@omesti.com>
2020-08-20 14:35:57 +0800

    add saved recipes model

--- masak2/users/models.py
+++ masak2/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
@@ -8,3 +8,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class SavedRecipe(models.Model):
+    """
+    This model store recipes that is saved by user
+    """
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="saved_recipes"
+    )
+    recipes = models.ManyToManyField("recipes.Recipe")
+
+
+def create_saved_recipes(sender, **kwargs):
+    """
+    Create `SavedRecipe` if and only if user is created
+    """
+    user = kwargs["instance"]
+    is_created = kwargs["created"]
+
+    if is_created:
+        SavedRecipe.objects.create(user=user)
+
+
+post_save.connect(create_saved_recipes, User)
