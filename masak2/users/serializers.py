79b25cf2bda125b9a9113a0260f41c26813a9ca8
syafiq <syafiq.termizi@omesti.com>
2020-08-20 23:21:46 +0800

    add API endpoint to add saved recipe

--- masak2/users/serializers.py
+++ masak2/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from recipes.models import Recipe
+
 from .models import SavedRecipe
 
 
@@ -7,3 +9,20 @@ class SavedRecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SavedRecipe
         fields = "__all__"
+
+
+class AddSavedRecipeSerializer(serializers.Serializer):
+    recipe_id = serializers.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        self.recipe = None
+        return super().__init__(*args, **kwargs)
+
+    def validate_recipe_id(self, value):
+        try:
+            recipe = Recipe.objects.get(pk=value)
+        except Recipe.DoesNotExist:
+            raise serializers.ValidationError("Not found", code="404")
+        else:
+            self.recipe = recipe
+        return value
--- masak2/users/views.py
+++ masak2/users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LogoutView
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import HttpResponseRedirect, render
-from rest_framework import generics
 
-from .serializers import SavedRecipeSerializer
+from rest_framework import generics, response
+
+from recipes.models import Recipe
+
+from .serializers import SavedRecipeSerializer, AddSavedRecipeSerializer
 from .models import SavedRecipe
 from .forms import LoginForm
 
@@ -29,3 +32,15 @@ class SavedRecipeViewAPIView(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "user_id"
     serializer_class = SavedRecipeSerializer
     queryset = SavedRecipe.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        """
+        This method is used to add recipe to SavedRecipe. It accepts request.data
+        as follows:
+        {'recipe_id': <id>}
+        """
+        serializer = AddSavedRecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = self.get_object()
+        instance.recipes.add(serializer.recipe)
+        return response.Response(data=SavedRecipeSerializer(instance=instance).data)
