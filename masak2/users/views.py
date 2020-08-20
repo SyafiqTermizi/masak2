9be43bf02bbe0d65f8c1f53997d3f6b92c9de8d2
syafiq <syafiq.termizi@omesti.com>
2020-08-20 23:26:31 +0800

    add permission

--- masak2/users/views.py
+++ masak2/users/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import HttpResponseRedirect, render
 
 from rest_framework import generics, response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from recipes.models import Recipe
 
@@ -32,6 +33,7 @@ class SavedRecipeViewAPIView(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "user_id"
     serializer_class = SavedRecipeSerializer
     queryset = SavedRecipe.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def update(self, request, *args, **kwargs):
         """
@@ -39,8 +41,11 @@ class SavedRecipeViewAPIView(generics.RetrieveUpdateAPIView):
         as follows:
         {'recipe_id': <id>}
         """
+
         serializer = AddSavedRecipeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         instance = self.get_object()
         instance.recipes.add(serializer.recipe)
+
         return response.Response(data=SavedRecipeSerializer(instance=instance).data)
