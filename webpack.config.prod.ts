f763a4eae764d19f2bc490a4e78b5e5bc678b5ec
syafiq <syafiq.termizi@omesti.com>
2020-08-26 18:55:10 +0800

    add url loader to load fonts

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -28,6 +28,10 @@ const config: webpack.Configuration = {
         test: /\.css$/,
         loader: "style-loader!css-loader",
       },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000",
+      },
     ],
   },
   resolve: {
