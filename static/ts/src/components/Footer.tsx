f3882bac75be5ec1380ccbda6530f9be4a6fc276
syafiq <syafiq.termizi@omesti.com>
2020-08-26 21:13:07 +0800

    add brand icon

--- static/ts/src/components/Footer.tsx
+++ static/ts/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faGithubAlt, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => (
   <footer
-    className="footer"
+    className="footer navbar"
     style={{
       backgroundColor: "lightgray",
       height: "35px",
@@ -17,7 +18,12 @@ export const Footer = () => (
           Made with <FontAwesomeIcon icon={faHeart} /> by Syafiq Termizi
         </div>
         <div className="col-4 text-right">
-          <a href="https://github.com/syafiqtermizi">Github</a>
+          <a className="mr-2 mt-5" href="https://github.com/syafiqtermizi">
+            <FontAwesomeIcon icon={faGithubAlt} />
+          </a>
+          <a href="https://www.linkedin.com/in/ahmad-syafiq-748291155/">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
         </div>
       </div>
     </div>
